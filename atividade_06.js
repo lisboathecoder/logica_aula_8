cfd21d06681422b8ea8f692a1232fe33701891d3
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-25 10:26:54 -0300

    commit

--- atividade_06.js
+++ atividade_06.js
@@ -1,7 +1,7 @@
 let itens = ["Item 1", "Item 2", "Item 3", "Item 4"];
+
 quantidadeItens = itens.length;
 
-for (let i = 0; i < quantidadeItens; i++) {
-  console.log(itens.reverse()[i]);
-  itens.reverse();
+for (let i = quantidadeItens - 1; i >= 0; i--) {
+  console.log(itens[i]);
 }
