5b607ad946de3ba0322e5f9db9bc38f0d3b1131b
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-25 11:17:15 -0300

    commit

---
+++ atividade_09.js
@@ -0,0 +1,8 @@
+let frutas = ["Banana", "Uva", "Maçã", "Abacaxi", "Melancia", "Morango"];
+
+quatidadeFrutas = frutas.length;
+
+console.log(`São ${quatidadeFrutas} frutas ao total:`);
+for (i = 0; i < quatidadeFrutas; i++) {
+  console.log(frutas[i]);
+}
