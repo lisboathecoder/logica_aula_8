8364d5b70eef93f0885531f16005dcd228eabfdd
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-25 09:29:20 -0300

    commit

---
+++ atividade_02.js
@@ -0,0 +1,11 @@
+let playlist = [
+  "Olha o kit - Bradockdan",
+  "NATA - iluvped, Dnovaees",
+  "PTSD - Nemzzz",
+  "Brota no Baile do Panta - MC Nego Trufa",
+];
+let quatidade = playlist.length;
+
+for (let i = 0; i < quatidade; i++) {
+  console.log(playlist[i]);
+}
