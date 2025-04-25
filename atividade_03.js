d60ee23f55b49cf5ca6f1e266ea9fbba9083bdf0
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-25 09:32:34 -0300

    commit

---
+++ atividade_03.js
@@ -0,0 +1,7 @@
+let amigos = ["Faibo", "Pedro", "João"]; // Aqui eu errei o nome do Fabio
+
+console.log(amigos); // Exibi com o nome errado
+
+amigos[0] = "Fabio"; // Corrigi o nome
+
+console.log(amigos); // Exibi o nome certo
