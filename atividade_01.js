3b1bcdb8032034df78fb18c0c443dc26a523c938
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-25 09:25:22 -0300

    commit

--- atividade_01.js
+++ atividade_01.js
@@ -1,2 +1,9 @@
-let filmes = ["Homem de Ferro", "Homem Aranha", "Capitão America", "Hulk"];
+let elenco = [
+  "Cristiano Ronaldo no Real Madrid",
+  "Neymar no Barcelona",
+  "Messi no Barcelona",
+  "Ronaldo na Copa de 2002",
+  "Ronaldinho Gaúcho na Copa de 2002",
+];
 
+console.log(elenco);
